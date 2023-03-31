6c705b49055783543438ec1f044cfe1c2b5413c3
nodarchik <chikadzee@gmail.com>
2023-03-31 14:48:55 +0400

    added cards and search function

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,92 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import q from "./data/quizes.json";
+import { ref, watch } from "vue";
+
+export default {
+  name: "QuizesComponent",
+  setup() {
+    const quizes = ref(q);
+    const search = ref("");
+
+    watch(search, (value) => {
+      quizes.value = q.filter((quiz) => {
+        return quiz.name.toLowerCase().includes(value.toLowerCase());
+      });
+    });
+
+    return { quizes, search };
+  },
+};
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model.trim="search" type="text" />
+    </header>
+    <div class="options-container">
+      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt=""/>
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.length }}questions</p>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
 header {
-  line-height: 1.5;
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+/* CARD */
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+.card img {
+  width: 100%;
+  height: 190px;
+  object-fit: cover;
+  margin: 0;
+}
+.card .card-text {
+  padding: 0 5px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.card .card-text h2 {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin: 10px 0;
 }
 </style>
